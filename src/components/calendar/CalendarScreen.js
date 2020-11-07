f50c8ef510298417fb372939fd9600154f912871
lockinDev <javieralmario12@gmail.com>
2020-11-07 15:04:09 -0400

    added bigcalendar library and its styles to the CalendarScreen file.

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Calendar, momentLocalizer }  from 'react-big-calendar'
+import moment from 'moment'
+
 import { Navbar } from '../ui/Navbar'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+
+ const localizer = momentLocalizer(moment);
+
+ const events = [{
+    title : 'First event', start : moment().toDate(), 
+    end : moment().add( 2, 'hours' ).toDate(), 
+    bgcolor : '#fafafa' //this is a own property
+ }];
 
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className = "calendar-screen">
             <Navbar />
+
+            <Calendar 
+                localizer = {localizer}
+                events = {events}
+                startAccessor = "start"
+                endAccesor = "end"
+            />
+
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CalendarApp from './CalendarApp';
-import './index.css';
+import './styles.css';
 
 
 ReactDOM.render(
