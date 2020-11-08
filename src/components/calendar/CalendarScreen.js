8b049cc7a2d74ca9dc49afe01b091e1621826fea
lockinDev <javieralmario12@gmail.com>
2020-11-08 19:16:45 -0400

    small modification of agenda message styles.

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -16,6 +16,25 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
  }];
 
 export const CalendarScreen = () => {
+
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+
+        console.log(event, start, end, isSelected);
+
+        const style = {
+            backgroundColor : '#367CF7', 
+            borderRadius : '0px', 
+            opacity : 0.8, 
+            display: 'block', 
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    };
+
     return (
         <div className = "calendar-screen">
             <Navbar />
@@ -25,6 +44,7 @@ export const CalendarScreen = () => {
                 events = {events}
                 startAccessor = "start"
                 endAccesor = "end"
+                eventPropGetter = { eventStyleGetter }
             />
 
         </div>
