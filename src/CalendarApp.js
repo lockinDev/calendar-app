73584c233b4d7d0d775c191c79ff1d5ddd6f8308
lockinDev <javieralmario12@gmail.com>
2020-11-19 23:48:42 -0400

    The redux, react-redux and redux-thunk libraries for state management are added.
    * Added modules that will handle states separated in folders.
    * Added first reducer and its types, uiReducer.
    * Created store to combine future reducers.

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+
+import { store } from './store/store';
 import { AppRouter } from './components/router/AppRouter';
 
 
 const CalendarApp = () =>(
-    <AppRouter />
+    <Provider store={ store}>
+        <AppRouter />
+    </Provider>
+    
 );
 
 export default CalendarApp; 
